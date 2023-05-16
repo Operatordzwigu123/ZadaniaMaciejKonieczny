0779b497086dfbff5997cf3a8697f12f90014f8d
Operatordzwigu123 <133816163+Operatordzwigu123@users.noreply.github.com>
2023-05-16 22:45:19 +0200

    Add files via upload

---
+++ Zadania/Zadanie 6.10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+struct Ochotnik
+{
+    public string nazwisko;
+    public double punktyMatematyka;
+    public double punktyInformatyka;
+    public double punktyJezykObcy;
+    public double Obliczpunktacje()
+ {
+ return punktyMatematyka * 0.6 + punktyInformatyka * 0.5 + punktyJezykObcy * 0.2;
+ }}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ochotnik[] kandydaci = new Ochotnik[3];
+        kandydaci[0].nazwisko = "Pudzianowski";
+        kandydaci[0].punktyMatematyka = 40;
+        kandydaci[0].punktyInformatyka = 50;
+        kandydaci[0].punktyJezykObcy = 90;
+        kandydaci[1].nazwisko = "Panacleti";
+        kandydaci[1].punktyMatematyka = 75;
+        kandydaci[1].punktyInformatyka = 80;
+        kandydaci[1].punktyJezykObcy = 85;
+        kandydaci[2].nazwisko = "Alibaba";
+        kandydaci[2].punktyMatematyka = 45;
+        kandydaci[2].punktyInformatyka = 60;
+        kandydaci[2].punktyJezykObcy = 55;
+
+        Console.WriteLine("Lista ochotników:");
+        foreach (Ochotnik kandydat in kandydaci)
+        {
+            double lacznaLiczbaPunktow = kandydat.Obliczpunktacje();
+            Console.WriteLine("{0}: {1} punktów", kandydat.nazwisko, lacznaLiczbaPunktow);
+        }
+    }
+}
---
+++ Zadania/Zadanie 6.8/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Prostopadloscian
+{
+    private double dlugosc;
+    private double szerokosc;
+    private double wysokosc;
+    public Prostopadloscian(double dlugosc, double szerokosc, double wysokosc)
+    {
+        this.dlugosc = dlugosc;
+        this.szerokosc = szerokosc;
+        this.wysokosc = wysokosc;
+    }
+
+    public double ObliczObjetosc()
+    {
+        return dlugosc * szerokosc * wysokosc;
+    }
+    public int zestawienieobjetosci(Prostopadloscian p)
+    {
+        double objetosc1 = this.ObliczObjetosc();
+        double objetosc2 = p.ObliczObjetosc();
+
+        if (objetosc1 > objetosc2)
+        {
+            return 1;
+        }
+        else if (objetosc1 < objetosc2)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Prostopadloscian p1 = new Prostopadloscian(2, 5, 6);
+        Prostopadloscian p2 = new Prostopadloscian(5, 7, 8);
+        Console.WriteLine("Objetosc p1: " + p1.ObliczObjetosc());
+        Console.ReadKey();
+        Console.WriteLine("Objetosc p2: " + p2.ObliczObjetosc());
+        Console.ReadKey();
+        int wynikPorownania = p1.zestawienieobjetosci(p2);
+        if (wynikPorownania == 1)
+        {
+            Console.WriteLine("P1 ma większą objętość niż P2.");
+            Console.ReadKey();
+        }
+        else if (wynikPorownania == -1)
+        {
+            Console.WriteLine("P2 ma większą objętość niż P1.");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("Oba prostopadłościany są takiej samek objętości.");
+            Console.ReadKey();
+        }
+    }
+}
